9e03a5b27bf17502758dfca750ab4487a4430a5c
NgocVuMinh <ngc.vux@gmail.com>
2025-11-21 21:35:22 +0100

    input_dim 76 -> 42

--- mimic3models/keras_models/lstm.py
+++ mimic3models/keras_models/lstm.py
@@ -9,7 +9,7 @@ class Network(Model):
 
     def __init__(self, dim, batch_norm, dropout, rec_dropout, task,
                  target_repl=False, deep_supervision=False, num_classes=1,
-                 depth=1, input_dim=76, **kwargs):
+                 depth=1, input_dim=42, **kwargs):
 
         print("==> not used params in network class:", kwargs.keys())
 
